9ececd4bb7b608c383bc0ebc43dd70f4885d0756
Julio Machado <jsmacbusiness@gmail.com>
2023-06-14 15:24:11 -0300

    Feat: adding data with react

--- 5 - Requisições HTTP com React/Requisições-HTTP-React/src/App.jsx
+++ 5 - Requisições HTTP com React/Requisições-HTTP-React/src/App.jsx
@@ -6,6 +6,8 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // 1 - resgatando dados
   useEffect(() => {
@@ -19,6 +21,24 @@ function App() {
     fechData();
   }, []);
 
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  };
+
   return (
     <>
       <h1>Lista de Produtos:</h1>
@@ -29,6 +49,29 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <h3>Nome:</h3>
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            <h3>Preço:</h3>
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </>
   );
 }
