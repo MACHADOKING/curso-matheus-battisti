16d57bc90d7f2bae91bb2e27d94b16ede30ff195
Julio Machado <jsmacbusiness@gmail.com>
2023-06-14 14:54:15 -0300

    Feat: retrieving API data

--- 5 - Requisições HTTP com React/Requisições-HTTP-React/src/App.jsx
+++ 5 - Requisições HTTP com React/Requisições-HTTP-React/src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fechData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fechData();
+  }, []);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>Lista de Produtos:</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
