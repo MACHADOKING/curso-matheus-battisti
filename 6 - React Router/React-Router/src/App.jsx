6c0a39a1f1858de417f65dad398b3e4d0279bc97
Julio Machado <jsmacbusiness@gmail.com>
2023-06-15 17:16:01 -0300

    Feat: url redirect

--- 6 - React Router/React-Router/src/App.jsx
+++ 6 - React Router/React-Router/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 // 1 - config react router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 // components
 import Navbar from "./components/Navbar";
@@ -30,6 +30,8 @@ function App() {
           <Route path="/products/:id" element={<Product />} />
           {/* 9 - search */}
           <Route path="/search" element={<SearchPage />} />
+          {/* 10 - redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
           {/* 7 - no match route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
