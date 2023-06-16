444e775420d9d282eb75f6f1f14fb5de837c585a
Julio Machado <jsmacbusiness@gmail.com>
2023-06-16 13:32:08 -0300

    Feat: changing context

--- 7 - Context API/Context-API-React/src/pages/Contact.jsx
+++ 7 - Context API/Context-API-React/src/pages/Contact.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../components/ChangeCounter";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const { counter } = useContext(CounterContext);
+  return (
+    <>
+      <h1>Contact</h1>
+      <p>Valor do contador: {counter}</p>
+      {/* 3 - alterando valor */}
+      <ChangeCounter />
+    </>
+  );
 };
 
 export default Contact;
