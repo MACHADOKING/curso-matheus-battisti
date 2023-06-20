1fd0018b6ced6fdb65fdd19e404dba3f1ed8e587
Julio Machado <jsmacbusiness@gmail.com>
2023-06-20 16:29:50 -0300

    Feat: useReducer

--- 8 - Hooks - React/Hooks-React/src/components/HookUseState.jsx
+++ 8 - Hooks - React/Hooks-React/src/components/HookUseState.jsx
@@ -14,6 +14,16 @@ const HookUseState = () => {
   };
   console.log(name);
 
+  // 2 - useState e input
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // envio a uma API
+    console.log(age);
+  };
+
   return (
     <div>
       {/* 1 - useState */}
@@ -21,6 +31,17 @@ const HookUseState = () => {
       <p>Variavel: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mudar nome</button>
+      {/* 2 - useState e input */}
+      <p>Digite a sua idade:</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+      <p>Você tem {age} anos!</p>
       <hr />
     </div>
   );
