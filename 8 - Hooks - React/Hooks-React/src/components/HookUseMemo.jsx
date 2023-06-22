069895af1f37d779c86024ba834efebc205d5e49
Julio Machado <jsmacbusiness@gmail.com>
2023-06-22 13:27:28 -0300

    Feat: useMemo

---
+++ 8 - Hooks - React/Hooks-React/src/components/HookUseMemo.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect, useMemo } from "react";
+
+const HookUseMemo = () => {
+  const [number, setNumber] = useState(0);
+
+  //   const premiumNumber = ["0", "100", "200"];
+
+  const premiumNumber = useMemo(() => {
+    return ["0", "100", "200"];
+  }, []);
+
+  useEffect(() => {
+    console.log("Premium numbers foi alterado.");
+  }, [premiumNumber]);
+
+  return (
+    <div>
+      <h2>useMemo</h2>
+      <input
+        type="text"
+        onChange={(e) => setNumber(e.target.value)}
+        value={number}
+      />
+      {premiumNumber.includes(number) ? <p>Acertou o numero</p> : ""}
+      <hr />
+    </div>
+  );
+};
+
+export default HookUseMemo;
