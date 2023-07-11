b1e7306c2b1be4fc91dd5752c72b905870668c70
Julio Machado <jsmacbusiness@gmail.com>
2023-07-11 18:07:21 -0300

    Feat: variables with react and typescript

--- 9 - TypeScript - React/src/App.tsx
+++ 9 - TypeScript - React/src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
 
 function App() {
+  // 1 - Variaveis
+
+  const name: string = "Julio";
+  const age: number = 23;
+  const isWorking: boolean = true;
+
   return (
-    <>
-      <h1>Hello World!!</h1>
-    </>
+    <div className="App">
+      <h1>Typescript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está trabalhando!</p>}
+    </div>
   );
 }
 
