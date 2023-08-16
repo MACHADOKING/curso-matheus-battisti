7025f7b9eb65b6b12c9799bbae910fae463d8526
Julio Machado <jsmacbusiness@gmail.com>
2023-08-16 17:51:34 -0300

    Feat: Desestruturando props

--- 9 - TypeScript - React/src/App.tsx
+++ 9 - TypeScript - React/src/App.tsx
@@ -3,6 +3,10 @@ import "./App.css";
 // 4 - importação de componentes
 import FirstComponent from "./components/FirstComponent";
 
+// 5 - desestruturando props
+import SecondComponent from "./components/SecondComponent";
+import Destructuring from "./components/Destructuring";
+
 function App() {
   // 1 - Variaveis
 
@@ -23,6 +27,13 @@ function App() {
       {isWorking && <p>Está trabalhando!</p>}
       <h3>{userGreeting(name)}</h3>
       <FirstComponent />
+      <SecondComponent name="Júlio" />
+      <Destructuring
+        title="Primeiro post"
+        content="Algum conteúdo"
+        commentsQty={10}
+        tags={["ts", "js"]}
+      />
     </div>
   );
 }
