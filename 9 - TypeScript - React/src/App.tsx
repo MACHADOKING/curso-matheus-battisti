2c264b46ecd786328ac9109a2181bfc1926c66cf
Julio Machado <jsmacbusiness@gmail.com>
2023-08-16 17:30:37 -0300

    Feat: funções em componentes

--- 9 - TypeScript - React/src/App.tsx
+++ 9 - TypeScript - React/src/App.tsx
@@ -7,12 +7,18 @@ function App() {
   const age: number = 23;
   const isWorking: boolean = true;
 
+  // 2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}`;
+  };
+
   return (
     <div className="App">
       <h1>Typescript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking && <p>Está trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
