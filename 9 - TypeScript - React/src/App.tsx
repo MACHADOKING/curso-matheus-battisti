23e24fdd94c5cee375b90f0f863e1041018d8f73
Julio Machado <jsmacbusiness@gmail.com>
2023-08-18 18:03:55 -0300

    Feat: Context API

--- 9 - TypeScript - React/src/App.tsx
+++ 9 - TypeScript - React/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { createContext } from "react";
 
 // 4 - importação de componentes
 import FirstComponent from "./components/FirstComponent";
@@ -12,7 +13,16 @@ import State from "./components/State";
 
 // 8 - type
 type textOrNull = string | null;
-type fixed = "Isso" | "Ou" | "Aquilo";
+// type fixed = "Isso" | "Ou" | "Aquilo";
+
+// 9 - context
+interface IAppContext {
+  language: string;
+  framework: string;
+  projects: number;
+}
+
+export const AppContext = createContext<IAppContext | null>(null);
 
 function App() {
   // 1 - Variaveis
@@ -32,35 +42,44 @@ function App() {
 
   mySecondText = "opa";
 
-  const testandoFixed: fixed = "Isso";
+  // const testandoFixed: fixed = "Isso";
+
+  // 9 - context
+  const contextValue: IAppContext = {
+    language: "Javascript",
+    framework: "Express",
+    projects: 5,
+  };
 
   return (
-    <div className="App">
-      <h1>Typescript com React</h1>
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isWorking && <p>Está trabalhando!</p>}
-      <h3>{userGreeting(name)}</h3>
-      <FirstComponent />
-      <SecondComponent name="Júlio" />
-      <Destructuring
-        title="Primeiro post"
-        content="Algum conteúdo"
-        commentsQty={10}
-        tags={["ts", "js"]}
-        category={Category.TS}
-      />
-      <Destructuring
-        title="Primeiro post"
-        content="Algum conteúdo"
-        commentsQty={10}
-        tags={["ts", "js"]}
-        category={Category.P}
-      />
-      <State />
-      {myText && <p>Tem texto na variavel.</p>}
-      {mySecondText && <p>Tem texto na variavel.</p>}
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>Typescript com React</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && <p>Está trabalhando!</p>}
+        <h3>{userGreeting(name)}</h3>
+        <FirstComponent />
+        <SecondComponent name="Júlio" />
+        <Destructuring
+          title="Primeiro post"
+          content="Algum conteúdo"
+          commentsQty={10}
+          tags={["ts", "js"]}
+          category={Category.TS}
+        />
+        <Destructuring
+          title="Primeiro post"
+          content="Algum conteúdo"
+          commentsQty={10}
+          tags={["ts", "js"]}
+          category={Category.P}
+        />
+        <State />
+        {myText && <p>Tem texto na variavel.</p>}
+        {mySecondText && <p>Tem texto na variavel.</p>}
+      </div>
+    </AppContext.Provider>
   );
 }
 
